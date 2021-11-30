b2a5348ab58818eaa1e63a0792486b9b39e38ff7
Anon Ymous <ancientstraits@protonmail.com>
2021-11-30 01:19:56 +0000

    add

--- test-espeak.c
+++ test-espeak.c
@@ -6,14 +6,14 @@
 FILE* outwav = NULL;
 
 // Write 4 bytes to a file, least significant first
-static void wav_4byte(int value, FILE* fp) {
+static void wav_4byte(int value) {
 	for (int ix = 0; ix < 4; ix++) {
-		fputc(value & 0xff, fp);
+		fputc(value & 0xff, outwav);
 		value = value >> 8;
 	}
 }
 
-static int wav_open(FILE* fp, const char* path, int rate) {
+static int wav_open(const char* path, int rate) {
 	static unsigned char wave_hdr[44] = {
 		'R',  'I',  'F',  'F',
 		0x24, 0xf0, 0xff, 0x7f,
@@ -29,57 +29,84 @@ static int wav_open(FILE* fp, const char* path, int rate) {
 	
 	while (isspace(*path)) path++;
 
-	fp = fopen(path, "wb");
-	if (fp == NULL) {
+	outwav = fopen(path, "wb");
+	if (outwav == NULL) {
 		perror(path);
 		return 0;
 	}
 
-	fwrite(wave_hdr, 1, 24, fp);
-	wav_4byte(rate, fp);
-	wav_4byte(rate * 2, fp);
-	fwrite(&wave_hdr[32], 1, 12, fp);
+	fwrite(wave_hdr, 1, 24, outwav);
+	wav_4byte(rate);
+	wav_4byte(rate * 2);
+	fwrite(&wave_hdr[32], 1, 12, outwav);
 
 	return 1;
 }
 
-static void wav_close(FILE* fp) {
-	fflush(fp);
-	unsigned long size = ftell(fp);
+static void wav_close() {
+	fflush(outwav);
+	unsigned long size = ftell(outwav);
 
-	if (fseek(fp, 4, SEEK_SET) != -1)
-		wav_4byte(size - 8, fp);
+	if (fseek(outwav, 4, SEEK_SET) != -1)
+		wav_4byte(size - 8);
 	else
 		perror("Failed to write filesize");
 	
-	if (fseek(fp, 40, SEEK_SET) != -1)
-		wav_4byte(size - 44, fp);
+	if (fseek(outwav, 40, SEEK_SET) != -1)
+		wav_4byte(size - 44);
 	else
 		perror("Failed to write wav data size");
 	
-	fclose(fp);
-	fp = NULL;
+	fclose(outwav);
+	outwav = NULL;
 }
 
+static int samplerate;
+unsigned int samples_total = 0;
+unsigned int samples_split = 0;
+unsigned int samples_split_seconds = 0;
+unsigned int wavefile_count = 0;
+char fname[210];
 static int SynthCallback(short *wav, int numsamples, espeak_EVENT *events) {
-	if (outwav == NULL && !wav_open(outwav, "out.wav", espeakEVENT_SAMPLERATE))
-		return 1;
 	if (wav == NULL)
 		return 0;	
-	if (numsamples > 0)
-		fwrite(wav, numsamples*2, 1, outwav);
 	for (; events->type != 0; events++) {
 		switch(events->type) {
 		case espeakEVENT_SAMPLERATE:
-			
+			samplerate = events->id.number;
+			samples_split = samplerate * samples_split_seconds;
+			break;
+		case espeakEVENT_SENTENCE:
+			if ((samples_split > 0) && (samples_total > samples_split)) {
+				wav_close();
+				samples_total = 0;
+				wavefile_count++;
+			}
+			break;
+		default:
+			break;
 		}
+	}
+	if (outwav == NULL) {
+		if (samples_split > 0) {
+			// THIS NEVER HAPPENS
+			sprintf(fname, "%s_%.2d%s", "out.wav", wavefile_count + 1, ".wav");
+			if (!wav_open(fname, samplerate))
+				return 1;
+		} else if (!wav_open("out.wav", samplerate))
+			return 1;
+	}
+
+	if (numsamples > 0) {
+		samples_total += numsamples;
+		fwrite(wav, numsamples*2, 1, outwav);
 	}
 	return 0;
 }
 
 int main(int argc, char* argv[]) {
-	char voicename[] = {"English"}; // Set voice by its name
-	char text[] = {"Hello world!"};
+	char voicename[] = "English"; // Set voice by its name
+	char text[] = "Hello world!";
 	int buflength = 0, options = espeakINITIALIZE_DONT_EXIT;
 	unsigned int position = 0, position_type = 0, end_position = 0, flags = espeakCHARS_AUTO;
 
@@ -96,8 +123,7 @@ int main(int argc, char* argv[]) {
 	espeak_Synth(text, buflength, position, position_type, end_position, flags, identifier, user_data);
 
 	if (outwav != NULL)
-		wav_close(outwav);
+		wav_close();
 
 	return 0;
 }
-
